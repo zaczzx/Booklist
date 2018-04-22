3d07998cf2706b5f4f6c693cbe52b7e6d56e5a37
zac-zaoxuanzhou <zaczzx@163.com>
2018-04-21 22:22:01 -0500

    add container booklist and connect it to reducerbooks

---
+++ src/actions/index.js
@@ -0,0 +1,3 @@
+export function selectBook(book) {
+  console.log('selected a book');
+}
---
+++ src/containers/book-list.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import { selectBook } from '../actions/index';
+
+class BookList extends React.Component {
+  renderList() {
+    return this.props.books.map((book) => {
+      return (
+        <l1
+          key={book.title}
+          className="list-group-item">
+          {book.title}
+        </l1>
+      )
+    })
+  }
+
+  render () {
+    return (
+      <ul className="list-group col-sm-4">
+        {this.renderList()}
+      </ul>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    books: state.books
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  // whenever selectbook is called,
+  // the result should be passed to all of your reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+export default connect(mapStateToProps)(BookList);
